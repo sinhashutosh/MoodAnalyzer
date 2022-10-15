86019f35e3b661be0e3ffb71abc424d117d7e174
Ashutosh <ashu.cse.sln@gmail.com>
2022-10-15 21:16:30 +0530

    null pointer exception handled

--- MoodAnalyzer.java
+++ MoodAnalyzer.java
@@ -12,18 +12,24 @@ public class MoodAnalyzer {
     }
 
 
-    public String moodAnalyse() {
-        if (message.contains("sad")) {
-            return "SAD";
-        } else {
-            return "HAPPY";
+    public String moodAnalyse() throws NullPointerException {
+        try {
+            if (message.contains("sad")) {
+                return "SAD";
+            } else {
+                return "HAPPY";
+            }
+        } catch (Exception e) {
+            return "Happy";
         }
     }
 
     public static void main(String[] args) {
+        MoodAnalyzer mood = new MoodAnalyzer();
         MoodAnalyzer mood1 = new MoodAnalyzer("I am in sad mood");
         MoodAnalyzer mood2 = new MoodAnalyzer("I am in Any mood");
         System.out.println(mood1.moodAnalyse());
         System.out.println(mood2.moodAnalyse());
+        System.out.println(mood.moodAnalyse());
     }
 }
