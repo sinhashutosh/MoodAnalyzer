8648ae0e37016996aa73f917f0865e000e1b9477
Ashutosh <ashu.cse.sln@gmail.com>
2022-10-15 20:49:06 +0530

    Refactor the code to take

--- MoodAnalyzer.java
+++ MoodAnalyzer.java
@@ -1,7 +1,18 @@
 package Day21;
 
 public class MoodAnalyzer {
-    public String moodAnalyse(String message) {
+    String message;
+
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public MoodAnalyzer() {
+        this.message = message;
+    }
+
+
+    public String moodAnalyse() {
         if (message.contains("sad")) {
             return "SAD";
         } else {
@@ -10,8 +21,9 @@ public class MoodAnalyzer {
     }
 
     public static void main(String[] args) {
-    MoodAnalyzer mood = new MoodAnalyzer();
-        System.out.println(mood.moodAnalyse("I am in sad Mood"));
-        System.out.println(mood.moodAnalyse("I am in Any Mood"));
+        MoodAnalyzer mood1 = new MoodAnalyzer("I am in sad mood");
+        MoodAnalyzer mood2 = new MoodAnalyzer("I am in Any mood");
+        System.out.println(mood1.moodAnalyse());
+        System.out.println(mood2.moodAnalyse());
     }
 }
